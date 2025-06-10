3565f1901aa22134c49e111115e454b376d13fd8
Wuallan D'Avilla <helendavilla2@gmail.com>
2025-06-10 14:12:37 -0300

    ajuste frontend para deploy

--- frontend/src/services/quizService.js
+++ frontend/src/services/quizService.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-const PORT = 3000; // ou importe de alguma config se mudar
-const API = `${window.location.protocol}//${window.location.hostname}:${PORT}/api`;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function saveScore(user, pontos) {
-  return axios.post(`${API}/scores`, { user, pontos });
+  return axios.post(`${API_BASE_URL}/scores`, { user, pontos });
 }
 
 export async function fetchRanking() {
-  const res = await axios.get(`${API}/scores`);
+  const res = await axios.get(`${API_BASE_URL}/scores`);
   return res.data; // array de { user, pontos, timestamp }
 }
